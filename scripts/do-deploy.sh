4ac0073dabeba96a8720e62b85ff5796e1e11e3f
Theodor Shaytanov <brainenjii@gmail.com>
2018-09-16 22:08:11 +0300

    last travis update

--- scripts/do-deploy.sh
+++ scripts/do-deploy.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+rules=""
+functions=""
+for file in $(git diff --name-only $TRAVIS_COMMIT_RANGE)
+do
+  if [[ $file =~ ^database\.rules\.json$ ]]; then
+    rules=",database"
+  fi
+  if [[ $file =~ ^functions.* ]]; then
+    functions=",functions"
+  fi
+done
+
+firebase deploy --only hosting$rules$functions
